cd2c40e3a015f13981bdfdd6c2e64718ec971afb
Patrick Zhao <patrick.zhao1989@outlook.com>
2020-10-16 21:53:49 +1100

    initial commit

--- src/subscriber/src/App.vue
+++ src/subscriber/src/App.vue
@@ -1,5 +1,4 @@
 <template>
-	<img alt="Vue logo" src="./assets/logo.png" />
 	<Audience msg="Streaming subscriber" />
 </template>
 
@@ -15,13 +14,3 @@ export default defineComponent({
 })
 </script>
 
-<style lang="scss">
-#app {
-	font-family: Avenir, Helvetica, Arial, sans-serif;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	text-align: center;
-	color: #2c3e50;
-	margin-top: 60px;
-}
-</style>
--- src/subscriber/src/components/Audience.vue
+++ src/subscriber/src/components/Audience.vue
@@ -34,7 +34,7 @@
 
 <script lang="ts">
 import { defineComponent, reactive } from "vue"
-const AgoraRTC = require("agora-rtc-sdk")
+import * as AgoraRTC from "agora-rtc-sdk"
 
 export default defineComponent({
 	name: "Audience",
@@ -187,20 +187,3 @@ export default defineComponent({
 })
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-h3 {
-	margin: 40px 0 0;
-}
-ul {
-	list-style-type: none;
-	padding: 0;
-}
-li {
-	display: block;
-	margin: 0 10px;
-}
-a {
-	color: #42b983;
-}
-</style>
