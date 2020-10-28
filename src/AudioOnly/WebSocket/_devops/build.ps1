32dce704ba22f923b38fb38db11432eed16db9ac
Chris <christoment@gmail.com>
2020-10-28 12:24:02 +1100

    Point to aplus dev azure resources

--- src/AudioOnly/WebSocket/_devops/build.ps1
+++ src/AudioOnly/WebSocket/_devops/build.ps1
@@ -1,6 +1,7 @@
 $rootDir=$PSScriptRoot
 Set-Location $rootDir\..
 
-docker build -t clementchristopher.azurecr.io/aplus-audiostreaming-wss-server .
+docker build -t apluslivestreamtest.azurecr.io/aplus-audiostreaming-wss-server .
+#docker build -t clementchristopher.azurecr.io/aplus-audiostreaming-wss-server .
 
 # docker run --rm -p 8080:8080 clementchristopher.azurecr.io/aplus-audiostreaming-wss-server ls
